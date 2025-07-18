f05a43c0165678cbf00b0d6f0e95c5743e266c02
Joe Sustaric <joe8307@gmail.com>
2025-07-18 16:00:11 +1000

    a recursive solution - but I dont think I want this.

--- src/calculator.py
+++ src/calculator.py
@@ -29,19 +29,25 @@ class TermDepositDetails:
         self.interest = interest
         self.term = term
         self.interest_freq = interest_freq
+        self.total_interest = deposit
 
 def calculate_interest_total(term_dep_details: TermDepositDetails) -> int:
 
+    # total_iterations = term_dep_details.term * ITERATIONS_PER_YEAR[InterestFreq.MONTH]
+    # running_total = term_dep_details.deposit
+
+    # while total_iterations != 0:
+    #     running_total += _calc_interest_chunk(running_total, term_dep_details)
+    #     total_iterations-= 1
+
+    # rounded_dec = Decimal(running_total).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+    # return int(rounded_dec) - term_dep_details.deposit
+
     total_iterations = term_dep_details.term * ITERATIONS_PER_YEAR[InterestFreq.MONTH]
-    running_total = term_dep_details.deposit
+    foo(term_dep_details, total_iterations)
 
-    while total_iterations != 0:
-        running_total += _calc_interest_chunk(running_total, term_dep_details)
-        total_iterations-= 1
-
-    rounded_dec = Decimal(running_total).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+    rounded_dec = Decimal(term_dep_details.total_interest).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
     return int(rounded_dec) - term_dep_details.deposit
-
 
 def _calc_interest_chunk(previous_total, term_dep_details: TermDepositDetails):
 
@@ -51,3 +57,18 @@ def _calc_interest_chunk(previous_total, term_dep_details: TermDepositDetails):
     result = daily_interest * DAYS_PER[term_dep_details.interest_freq]
 
     return result
+
+
+def foo(term_dep_details: TermDepositDetails, counter)-> TermDepositDetails:
+
+    if counter == 0:
+        return
+
+    raw_interest = (term_dep_details.total_interest * term_dep_details.interest)
+    daily_interest = (raw_interest / DAYS_PER[InterestFreq.YEAR])
+
+    term_dep_details.total_interest += daily_interest * DAYS_PER[term_dep_details.interest_freq]
+
+    counter -= 1
+    foo(term_dep_details, counter)
+

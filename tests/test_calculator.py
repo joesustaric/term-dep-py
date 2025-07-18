cc3a8f177a3f1d10fd7e17b40defbaf59bf0a039
Joe Sustaric <joe8307@gmail.com>
2025-07-18 23:03:42 +1000

    adding in more test cases for the 10000k 1.1% interest 3 years case

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,9 @@
 from src import calculator
 
 
-def test_calculate_interest(capsys):
+def test_calc_interest_deposit_10000_interest_1_point_1_monthly_term_3(capsys):
     deposit, interest, term = 10000, 0.011, 3
+
     term_dep_details = calculator.TermDepositDetails(
         deposit,
         interest,
@@ -13,5 +14,53 @@ def test_calculate_interest(capsys):
     interest_result = calculator.calculate_interest_total(term_dep_details)
 
     correct_interest = 335
+
+    assert correct_interest == interest_result
+
+def test_calc_interest_deposit_10000_interest_1_point_1_quarterly_term_3(capsys):
+    deposit, interest, term = 10000, 0.011, 3
+
+    term_dep_details = calculator.TermDepositDetails(
+        deposit,
+        interest,
+        term,
+        calculator.InterestFreq.QUARTER
+        )
+
+    interest_result = calculator.calculate_interest_total(term_dep_details)
+
+    correct_interest = 335
+
+    assert correct_interest == interest_result
+
+def test_calc_interest_deposit_10000_interest_1_point_1_annually_term_3(capsys):
+    deposit, interest, term = 10000, 0.011, 3
+
+    term_dep_details = calculator.TermDepositDetails(
+        deposit,
+        interest,
+        term,
+        calculator.InterestFreq.ANNUAL
+        )
+
+    interest_result = calculator.calculate_interest_total(term_dep_details)
+
+    correct_interest = 334
+
+    assert correct_interest == interest_result
+
+def test_calc_interest_deposit_10000_interest_1_point_1_maturity_term_3(capsys):
+    deposit, interest, term = 10000, 0.011, 3
+
+    term_dep_details = calculator.TermDepositDetails(
+        deposit,
+        interest,
+        term,
+        calculator.InterestFreq.MATURITY
+        )
+
+    interest_result = calculator.calculate_interest_total(term_dep_details)
+
+    correct_interest = 330
 
     assert correct_interest == interest_result
